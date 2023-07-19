cdff43da66b760d4544a5e05d092fb36bda587ae
jamesparsaie <parsaiejames@gmail.com>
2023-07-18 21:03:37 -0500

    Create navigation hierarchy and detailed Scrum View

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -7,19 +7,31 @@
 
 import SwiftUI
 
+
 struct ScrumsView: View {
     let scrums: [DailyScrum]
-    
+  
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
-
-struct ScrumsView_Previews: PreviewProvider {
-    static var previews: some View {
-        ScrumsView(scrums: DailyScrum.sampleData)
+    
+    struct ScrumsView_Previews: PreviewProvider {
+        static var previews: some View {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
     }
-}
