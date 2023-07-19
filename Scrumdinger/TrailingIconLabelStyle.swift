532d1389e97b516c79c39cc72d2d4cba0416ef11
jamesparsaie <parsaiejames@gmail.com>
2023-07-18 20:21:57 -0500

    CardView and ScrumView completed

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Scrumdinger
+//
+//  Created by James Parsaie on 7/18/23.
+//
+
+import SwiftUI
+
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self {Self()}
+}
